ca07ba1472d2e12d3b4addbf1cf1525ca828bad3
daniel-guzdek <68995541+daniel-guzdek@users.noreply.github.com>
2025-02-15 12:02:51 +0100

    Switch to react-player

--- src/containers/Calculators/Calculators.jsx
+++ src/containers/Calculators/Calculators.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { calcCardsHeaderData, calculatorsHeaderData } from "../../data/headers";
 import CalcCard from "../../components/CalcCard/CalcCard";
 import Footer from "../../components/Footer/Footer";
+import ReactPlayer from "react-player";
 
 const Calculators = () => {
   const renderCalcCards = calcCardsHeaderData?.map((card, index) => {
@@ -18,16 +19,14 @@ const Calculators = () => {
   return (
     <>
       <div className="calculators-wrapper">
-        <video autoPlay loop muted src="../videos/Asteroid_1920px.mp4">
-          <source
-            media="(min-width: 1200px)"
-            srcSet="../videos/Asteroid_640px.mp4"
-          />
-          <source
-            media="(min-width: 640px)"
-            srcSet="../videos/Asteroid_340px.mp4"
-          />
-        </video>
+        <ReactPlayer
+          url="../videos/Asteroid_1920px.mp4"
+          playing
+          loop
+          muted
+          width="100%"
+          height="100%"
+        />
         <div className="calculators-container">
           <div className="calculators-content">
             <h1 className="calculators-title">
--- src/containers/Hero/Hero.jsx
+++ src/containers/Hero/Hero.jsx
@@ -1,33 +1,20 @@
 import React from "react";
-import "../../videos/Nebula_1920px.mp4";
-import "../../videos/Nebula_1280px.mp4";
-import "../../videos/Nebula_880px.mp4";
-import "../../videos/Nebula_640px.mp4";
 import { heroHeaderData } from "../../data/headers";
 import Footer from "../../components/Footer/Footer";
+import ReactPlayer from "react-player";
 
 const Hero = () => {
   return (
     <>
       <div className="hero-container">
-        <video autoPlay loop muted src="../../videos/Nebula_1920px.mp4">
-          <source
-            media="(min-width: 1600px)"
-            srcSet="../../videos/Nebula_1920px.mp4"
-          />
-          <source
-            media="(min-width: 1200px)"
-            srcSet="../../videos/Nebula_1280px.mp4"
-          />
-          <source
-            media="(min-width: 800px)"
-            srcSet="../../videos/Nebula_880px.mp4"
-          />
-          <source
-            media="(min-width: 300px)"
-            srcSet="../../videos/Nebula_640px.mp4"
-          />
-        </video>
+        <ReactPlayer
+          url="../../videos/Nebula_1920px.mp4"
+          playing
+          loop
+          muted
+          width="100%"
+          height="100%"
+        />
         <div className="hero-header">
           <h1 className="hero-title">{heroHeaderData?.title}</h1>
           <h2 className="hero-subtitle">{heroHeaderData?.subtitle}</h2>
