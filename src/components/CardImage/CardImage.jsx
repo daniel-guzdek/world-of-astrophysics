a5fe7f789b325cda8d68b730855d9cb87903dd1f
daniel-guzdek <68995541+daniel-guzdek@users.noreply.github.com>
2025-02-16 02:01:07 +0100

    Add small loader for card img

--- src/components/CardImage/CardImage.jsx
+++ src/components/CardImage/CardImage.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const CardImage = ({ src, title, icon, category }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (src) {
+      setIsLoading(true);
+    }
+  }, [src]);
+
+  const handleLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
     <div className="card-image-container">
-      {src ? (
-        <img src={src} alt={title} className="card-image" />
-      ) : (
+      {src && isLoading && <div class="loader"></div>}
+      {src && (
+        <img src={src} alt={title} className="card-image" onLoad={handleLoad} />
+      )}
+      {!src && (
         <div className="card-icon">
           <span>{icon}</span>
           <span>{category}</span>
